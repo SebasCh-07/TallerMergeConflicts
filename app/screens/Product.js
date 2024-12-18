fde2b73ecebdb49a2a12c7a3c1c90aea96fb2105
PaulRiveraC <paulriveracamacho@gmail.com>
2024-12-17 23:08:48 -0500

    cmmmit estudiante 1

--- app/screens/Product.js
+++ app/screens/Product.js
@@ -1,12 +1,15 @@
-import {View, Text, StyleSheet, Button} from 'react-native'
+import { View, Text, StyleSheet, Button } from 'react-native'
 
-export const Product = ({navigation}) =>{
-    return <View style={styles.container}> 
-        <Text>Estas en productos</Text>
+export const Product = ({ navigation }) => {
+    return <View style={styles.container}>
+        <Text>Bienvenido a mi tienda, soy: Paul</Text>
+        <Text style={styles.producto1}>CocaCola</Text>
+        <Text style={styles.producto2}>Sprit</Text>
+        <Text style={styles.producto3}>Pepsi</Text>
         <Button
             title='HOME'
-            onPress={()=>{
-                navigation.navigate('HomeNav');
+            onPress={() => {
+                navigation.navigate('WellcomeNav');
             }}
         />
     </View>
@@ -15,9 +18,18 @@ export const Product = ({navigation}) =>{
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
+        flex: 1,
+        backgroundColor: '#aeb6bf',
+        alignItems: 'center',
+        justifyContent: 'center',
     },
-  });
+    producto1: {
+        color: 'yellow'
+    },
+    producto2: {
+        color: 'blue'
+    },
+    producto3: {
+        color: 'red'
+    }
+});
